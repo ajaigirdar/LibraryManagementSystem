8fd8f1dec81231bd8551f6b35c994bad69a2cd92
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-01-27 12:36:28 -0500

    added add()

--- src/main/java/learn/library/data/BookRepository.java
+++ src/main/java/learn/library/data/BookRepository.java
@@ -6,15 +6,14 @@ import java.util.List;
 
 public interface BookRepository {
     // TODO: interface defining CRUD operations for a book
-//    Book add(Book book) throws DataAccessException;
-//
-//    boolean deleteById(int encounterId) throws DataAccessException;
-//
-//    boolean update(Encounter encounter) throws DataAccessException;
-//
-//    Encounter findByType(EncounterType type) throws DataAccessException;
+    //    Book add(Book book) throws DataAccessException;
+    //    boolean deleteById(int encounterId) throws DataAccessException;
+    //    boolean update(Encounter encounter) throws DataAccessException;
+    //    Encounter findByType(EncounterType type) throws DataAccessException;
 
     List<Book> findAll();
+
+    public boolean add(Book book) throws DataAccessException;
 
 
 }
