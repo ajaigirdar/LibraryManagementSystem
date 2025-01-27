6d88771833b75092cd38d84fb53c127cd4e9cafa
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-01-27 12:26:51 -0500

    added enum Bookcategory

--- src/main/java/learn/library/data/model/BookCategory.java
+++ src/main/java/learn/library/data/model/BookCategory.java
@@ -1,5 +1,12 @@
 package learn.library.data.model;
 
 public enum BookCategory {
-    // TODO: enum for storing types of books (Fantasy, Mystery, Si-Fi etc)
+    FANTASY,
+    MYSTERY,
+    SCI_FI,
+    NON_FICTION,
+    ROMANCE,
+    HISTORY,
+    CHILDREN,
+    REFERENCE;
 }
